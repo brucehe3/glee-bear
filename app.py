9c52a910ee1d8ca250128137211fda437bcca757
luo <luopanglaos@gmail.com>
2024-10-18 16:48:22 +0800

    init

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+from logic import recommend_for_me
+from streamlit_chat import message
+from streamlit_js_eval import get_geolocation
+
+st.header("GoGlee Bear")
+
+if st.checkbox("Check my location"):
+    location = get_geolocation()
+    st.session_state['location'] = location
+
+st.write("Just type: \"I want Chinese food\" or similar, and I'll recommend it for you!")
+
+# if not ('latitude' in location and location['latitude']):
+#     st.write("Please share your location to get started.")
+
+if not ("user_prompt_history" in st.session_state
+        and "chat_answers_history" in st.session_state
+        and "chat_history" in st.session_state
+        and "location" in st.session_state
+):
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_answers_history"] = []
+    st.session_state["chat_history"] = []
+    st.session_state["location"] = {}
+
+
+def shop_response(shops):
+    response = []
+    shop_html = ("<div style='position:relative; background-image:url({photo}); "
+                 "height:200px; width:240px; margin-bottom:10px;border-radius:20px;'>"
+                 "<div style='position: absolute; bottom:0; border-bottom-left-radius: 20px;"
+                 "border-bottom-right-radius: 20px; width:100%;"
+                 "background: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.6));'>"
+                 "<h3 style='margin:8px;'>{name}</h3>"
+                 "<p style='padding:4px 8px;font-size:14px'>{reason}</p>"
+                 "<p style='font-size:12px;padding:0 8px 8px 0; text-align:right'>{distance:.2f} km</p>"
+                 "</div>"
+                 "</div>")
+    for shop in shops:
+        response.append(shop_html.format(**shop))
+
+    return "".join(response)
+
+
+prompt = st.text_input("What would you like to eat?", placeholder="Enter your thoughts..")
+
+if prompt:
+
+    if not st.session_state["location"]:
+        st.write("Please share your location to get started.")
+    else:
+        with st.spinner("Generating response.."):
+            coords = st.session_state["location"].get("coords")
+            generated_response = recommend_for_me(
+                prompt, coords['latitude'], coords['longitude'], 5000, mock=False)
+
+            if "message" in generated_response:
+                st.write(generated_response["message"])
+            else:
+                formatted_response = shop_response(generated_response['shops'])
+
+                st.session_state["user_prompt_history"].insert(0, prompt)
+                st.session_state["chat_answers_history"].insert(0, formatted_response)
+                # st.session_state["chat_history"].append(("human", prompt))
+                # st.session_state["chat_history"].append(("ai", generated_response["result"]))
+
+if st.session_state["chat_answers_history"]:
+    i = 0
+    for user_query, generated_response in zip(st.session_state["user_prompt_history"],
+                                              st.session_state["chat_answers_history"]):
+        message(user_query, is_user=True, key=f"user_message_{i}")
+        message(generated_response, key=f"response_message_{i}", allow_html=True)
+        i += 1
---
+++ logic.py
@@ -0,0 +1,119 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers.json import JsonOutputParser
+from pydantic import BaseModel, Field
+from typing import List
+from geopy.distance import geodesic
+from lib.google_place import sample_search_nearby, get_photo
+from lib.google_place_type import restaurant
+from lib.mock.data import final_shops
+from lib.prompt_template import place_prompt_string, recommend_prompt_string
+
+load_dotenv()
+
+
+class PlaceType(BaseModel):
+    place_types: str = Field(description="matched items")
+
+
+class Shop(BaseModel):
+    index: int = Field(description="index number")
+    name: str = Field(description="shop name")
+    reason: str = Field(description="reason for recommendation")
+
+
+class ShopList(BaseModel):
+    shops: List[Shop]
+
+
+def format_shop(shop_list):
+    """
+    1. Shop A: Rating 4.5, Review: 'Great environment, friendly service, but the price is a bit high.'
+    2. Shop B: Rating 4.8, Review: 'Food is very delicious, prices are reasonable, but sometimes there is a wait.'
+    :param shop_list:
+    :return:
+    """
+    return "\n\n".join(
+        f"Index: {shop['index']}. {shop['name']}: Rating {shop['rating']}, Reviews: {shop['reviews']}" for shop in shop_list)
+
+
+def shop_result_parser(original_shops, shops, latitude, longitude):
+
+    shop_dict = {shop["index"]: shop for shop in original_shops}
+
+    for item in shops:
+        shop = shop_dict.get(item["index"])
+        item["google_map_uri"] = shop["google_map_uri"]
+        item["distance"] = geodesic((latitude, longitude), (shop["latitude"], shop["longitude"])).kilometers
+        if shop["photos"]:
+            item["photo"] = get_photo(shop["photos"][0].name)
+        else:
+            item["photo"] = ""
+
+    return shops
+
+
+def place_type_tool(prompt):
+    """from prompt understand customer's needs"""
+
+    parser = JsonOutputParser(pydantic_object=PlaceType)
+    prompt_template = PromptTemplate(
+        template=place_prompt_string,
+        input_variables=["user_input"],
+        partial_variables={
+            "place_type": restaurant,
+            "format_instructions": parser.get_format_instructions(),
+        }
+    )
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", timeout=10)
+    chain = prompt_template | llm | parser
+
+    result = chain.invoke(input={
+        "user_input": prompt
+    })
+
+    return result
+
+
+def recommend_for_me(prompt, latitude, longitude, radius, chat_history=[], mock=False):
+    """let llm to decide which shops should recommend me"""
+
+    if mock:
+        return final_shops
+
+    # from prompt to split business type first
+    place_result = place_type_tool(prompt)
+
+    if not place_result["place_types"]:
+        return {
+            "message": "Please enter your thoughts"
+        }
+
+    shops = sample_search_nearby(place_result["place_types"], latitude, longitude, radius, mock)
+    llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+
+    parser = JsonOutputParser(pydantic_object=ShopList)
+    # 创建 PromptTemplate 实例
+    prompt_template = PromptTemplate(
+        template=recommend_prompt_string,
+        input_variables=["action"],
+        partial_variables={
+            "context": format_shop(shops),
+            "format_instructions": parser.get_format_instructions(),
+        }
+    )
+    # 使用 LLM 生成结果
+    chain = prompt_template | llm | parser
+    result = chain.invoke(input={
+        "action": prompt
+    })
+
+    # result parser
+    result['shops'] = shop_result_parser(shops, result['shops'], latitude, longitude)
+    return result
+
+
+if __name__ == "__main__":
+    print(recommend_for_me("我想吃海南鸡饭", 1.3920613, 103.913496, 2000))
+    # print(place_type_tool("我想吃酸辣土豆丝"))
